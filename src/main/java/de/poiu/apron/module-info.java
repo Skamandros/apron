0858800808f8ee06eabfdadba890bffcc42a0df4
Marco Herrn <marco@mherrn.de>
2019-04-22 15:05:24 +0200

    Prepared modularization

---
+++ src/main/java/de/poiu/apron/module-info.java
@@ -0,0 +1,10 @@
+module apron {
+    requires java.logging;
+
+    exports de.poiu.apron;
+    exports de.poiu.apron.entry;
+    exports de.poiu.apron.escaping;
+    exports de.poiu.apron.io;
+    exports de.poiu.apron.reformatting;
+
+}
